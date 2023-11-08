8a7022a5bece390f45a66f5894ea8c63127e578b
Peter Sefton <pt@ptsefton.com>
2023-11-09 10:34:37 +1100

    Changed the way profile is passed in

--- test/generate_spec.spec.js
+++ test/generate_spec.spec.js
@@ -6,7 +6,9 @@ const fs = require("fs")
 describe("generateSpec", function () {
 
     it("Generates a sensible spec", function () {
-      const result = Profile.generateSpec("profiles/software-profile.json", "test_data/software-source-code/text/profile.md", "test_data/software-source-code/examples/");
+      const profile = require("../profiles/software-profile.json");
+
+      const result = Profile.generateSpec(profile, "test_data/software-source-code/text/profile.md", "test_data/software-source-code/examples/");
         // NOTE regexes below have . in place of [ ]
         assert(result.match(`-  MUST have at least the following types: ."Dataset","SoftwareSourceCode","SoftwareApplication".`))
         assert(result.match(`-  MUST have a conformsTo propery with least the following values .{"@id":"https://purl.archive.org/language-data-commons/profile#Software"}.`))
