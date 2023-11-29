ae86664d73b55dd4a7438deb24b982da434ea30a
Peter Sefton <pt@ptsefton.com>
2023-11-30 10:57:04 +1100

    Added SelectObject to tests

--- test/generate_spec.spec.js
+++ test/generate_spec.spec.js
@@ -11,12 +11,35 @@ describe("generateSpec", function () {
       const result = Profile.generateSpec(profile, "test_data/software-source-code/text/profile.md", "test_data/software-source-code/examples/");
         // NOTE regexes below have . in place of [ ]
         assert(result.match(`-  MUST have at least the following types: ."Dataset","SoftwareSourceCode","SoftwareApplication".`))
-        assert(result.match(`-  MUST have a conformsTo propery with least the following values .{"@id":"https://purl.archive.org/language-data-commons/profile#Software"}.`))
+        assert(result.match(`-  MUST have a conformsTo property with least the following values .{"@id":"https://purl.archive.org/language-data-commons/profile#Software"}.`))
         assert(result.match(/```json\n+/m))
 
-        fs.writeFileSync("test-profile.md", result)
+        fs.writeFileSync("test-software-profile.md", result)
 
     });
+
+
+
+    it("Can Deal with <select> in a mode file", function () {
+      const profile = require("../profiles/language-data-commons-collection-profile.json");
+      // Not worrying about examples in this one
+      const result = Profile.generateSpec(profile, "test_data/ldac-profile/profile/text/profile.md", "test_data/software-source-code/examples/");
+        // NOTE regexes below have . in place of [ ]
+        fs.writeFileSync("test-ldac-profile.md", result)
+
+        assert(result.match(`-  MUST have at least the following types: ."Dataset","RepositoryCollection".`))
+        assert(result.match(`-  MUST have a conformsTo property with least the following values .*{"@id":"https://purl.archive.org/language-data-commons/profile#Collection"}.`))
+       
+        assert(result.match(/\<a name='object-txc_ElicitationTask'\/\>/))
+        assert(result.match(/.*###  Value ElicitationTask/))
+        assert(result.match(/The collection protocol includes a task-based prompt to participants.*/))
+
+
+    });
+
+
+    
+
 
 
 
